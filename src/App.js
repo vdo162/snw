6236f097c43bb76a299930e1c1cece1090e8fd63
vdo162 <vdo162@gmail.com>
2021-03-08 18:15:04 +0300

    added hashrouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {BrowserRouter, Route, withRouter} from "react-router-dom";
+import {HashRouter, Route, withRouter} from "react-router-dom";
 import UsersContainer from "./components/Users/UsersContainer";
 import LoginPage from "./components/Login/Login"
 import React, {Component} from "react";
@@ -61,10 +61,10 @@ const AppContainer = compose(
     connect (mapStateToProps, {initializeApp})) (App);
 
 const MainApp = () => {
-    return <BrowserRouter>
+    return <HashRouter>
         <Provider store={store}>
             <AppContainer/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 }
 export default MainApp;
